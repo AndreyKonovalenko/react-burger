f5ab6cbb28da92aef38b77a777d8322544c6d459
AndreyKonovalenko <andreinew@mail.ru>
2023-04-01 18:13:35 +0300

    chore: rewrote AppHeader class to functional component

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -7,33 +7,25 @@ export const BURGER_CONSTRUCTOR = 'Конструктор';
 export const ORDER_FEED = 'Лента заказов';
 export const USER_ACCOUNT = 'Личный кабинет';
 
-class AppHeader extends React.Component {
-  render() {
-    return (
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={`${styles.itemContainer} ${styles.decorator1}`}>
-            <NavItem
-              option={BURGER_CONSTRUCTOR}
-              onClick={() => {}}
-              isActive={true}
-            />
-            <NavItem option={ORDER_FEED} onClick={() => {}} isActive={false} />
-          </div>
-          <div className={`${styles.itemContainer} ${styles.decorator2}`}>
-            <Logo />
-          </div>
-          <div className={`${styles.itemContainer} ${styles.decorator3}`}>
-            <NavItem
-              option={USER_ACCOUNT}
-              onClick={() => {}}
-              isActive={false}
-            />
-          </div>
-        </nav>
-      </header>
-    );
-  }
-}
+const AppHeader = () => (
+  <header className={styles.header}>
+    <nav className={styles.nav}>
+      <div className={`${styles.itemContainer} ${styles.decorator1}`}>
+        <NavItem
+          option={BURGER_CONSTRUCTOR}
+          onClick={() => {}}
+          isActive={true}
+        />
+        <NavItem option={ORDER_FEED} onClick={() => {}} isActive={false} />
+      </div>
+      <div className={`${styles.itemContainer} ${styles.decorator2}`}>
+        <Logo />
+      </div>
+      <div className={`${styles.itemContainer} ${styles.decorator3}`}>
+        <NavItem option={USER_ACCOUNT} onClick={() => {}} isActive={false} />
+      </div>
+    </nav>
+  </header>
+);
 
 export default AppHeader;
