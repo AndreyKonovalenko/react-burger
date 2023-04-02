498468d2ed9eea9238fe71dee9f080112c237dc1
AndreyKonovalenko <andreinew@mail.ru>
2023-04-02 21:29:20 +0300

    chore: aded basis fetch ingredient logic

--- src/components/constructor-container/constructor-container.js
+++ src/components/constructor-container/constructor-container.js
@@ -1,69 +1,87 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './constructor-container.module.css';
 import BurgerConstructor from './burger-constructor/burger-constructor';
 import BurgerIngredients from './burger-ingredients/burger-ingredients';
 
-import { data } from '../../utils/data';
+// import { data } from '../../utils/data';
 import Modal from '../modal/modal';
 
 export const BUN = 'Булки';
 export const SAUCE = 'Соусы';
 export const MAIN = 'Начинки';
+const URL = 'https://norma.nomoreparties.space/api/ingredients';
 
-class ConstructorContainer extends React.Component {
-  state = {
-    current: BUN,
-    visible: false,
-    burger: {
-      top: '60666c42cc7b410027a1a9b1',
-      bottom: '60666c42cc7b410027a1a9b1',
-      rest: [
-        '60666c42cc7b410027a1a9b5',
-        '60666c42cc7b410027a1a9b6',
-        '60666c42cc7b410027a1a9b7',
-        '60666c42cc7b410027a1a9b4',
-        '60666c42cc7b410027a1a9b9',
-        '60666c42cc7b410027a1a9b8',
-        '60666c42cc7b410027a1a9bc',
-        '60666c42cc7b410027a1a9bb',
-      ],
-    },
-  };
+const ConstructorContainer = () => {
+  const [current, setCurrent] = useState(BUN);
+  const [visible, setVisible] = useState(false);
+  const [burger, setBurger] = useState({
+    top: '60d3b41abdacab0026a733c6',
+    bottom: '60d3b41abdacab0026a733c6',
+    rest: [
+      '60d3b41abdacab0026a733c8',
+      '60d3b41abdacab0026a733c8',
+      '60d3b41abdacab0026a733c8',
+    ],
+  });
+  const [data, setData] = useState(null);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  handleTabSelect = (value) => {
-    this.setState({ current: value });
+  const handleTabSelect = (value) => {
+    setCurrent(value);
     const element = document.getElementById(value);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
-  handleOpenModal = () => {
-    this.setState({ visible: true });
+  const handleOpenModal = () => {
+    setVisible(true);
   };
-  handleCloseModal = () => {
-    console.log('click');
-    this.setState({ visible: false });
+  const handleCloseModal = () => {
+    setVisible(false);
   };
 
-  render() {
-    console.log(this.state.visible);
-    const { current, burger } = this.state;
-    return (
-      <main className={styles.main}>
-        <BurgerIngredients
-          handleTabSelect={this.handleTabSelect}
-          handleOpenModal={this.handleOpenModal}
-          handleCloseModal={this.handleCloseModal}
-          visible={this.visible}
-          current={current}
-          data={data}
-        />
-        <BurgerConstructor burger={burger} data={data} />
-        {this.state.visible && <Modal onClose={this.handleCloseModal} />}
-      </main>
-    );
-  }
-}
+  useEffect(() => {
+    console.log(data, isLoading);
+    const fetchData = (url) => {
+      setIsLoading(true);
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          setData(res.data);
+          setIsLoading(false);
+        })
+        .catch((e) => {
+          setHasError(true);
+          isLoading(false);
+          console.log(e);
+        });
+    };
+
+    if (data === null && isLoading === false) {
+      fetchData(URL);
+    }
+  }, [data, isLoading]);
+
+  return (
+    <main className={styles.main}>
+      {data && isLoading === false ? (
+        <>
+          <BurgerIngredients
+            handleTabSelect={handleTabSelect}
+            handleOpenModal={handleOpenModal}
+            handleCloseModal={handleCloseModal}
+            visible={visible}
+            current={current}
+            data={data}
+          />
+          <BurgerConstructor burger={burger} data={data} />
+        </>
+      ) : null}
+      {visible && <Modal onClose={handleCloseModal} />}
+    </main>
+  );
+};
 
 export default ConstructorContainer;
